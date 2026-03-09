0dead75a22609193ac18103eccf3eaa8ed58d7b3
JuanBonet23 <168552918+JuanBonet23@users.noreply.github.com>
2026-03-09 15:43:19 -0500

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,8 @@
-export const firebaseConfig = {
-  apiKey: "AIza....",
+const firebaseConfig = {
+  apiKey: "AIzaSyChYKMos8gmz0MuhWJPjj8sovwamJ-fMt4",
   authDomain: "bonet-23.firebaseapp.com",
   projectId: "bonet-23",
-  storageBucket: "bonet-23.appspot.com",
+  storageBucket: "bonet-23.firebasestorage.app",
   messagingSenderId: "756455673447",
-  appId: "1:756455673447:web:xxxx"
+  appId: "1:756455673447:web:25acb29782a80dade28ec2"
 };
